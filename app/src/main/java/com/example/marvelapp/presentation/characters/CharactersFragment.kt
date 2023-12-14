0f41a29b4b914a2103b04363122edf3bcd9a1625
kadusouza <kdusouza@gmail.com>
2023-12-14 18:06:40 -0300

    Melhora organização do projeto

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,15 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.core.domain.model.Character
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private lateinit var binding: FragmentCharactersBinding
+
+    private val charactersAdapter = CharactersAdapter()
+
+    //private var _binding: FragmentCharactersBinding? = null
+    //private val binding: FragmentCharactersBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ) = FragmentCharactersBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf(
+                Character("3D-Man", "https://pbs.twimg.com/media/EoQClRBXYAECWxN.jpg"),
+                Character("3D-Man", "https://pbs.twimg.com/media/EoQClRBXYAECWxN.jpg"),
+                Character("3D-Man", "https://pbs.twimg.com/media/EoQClRBXYAECWxN.jpg"),
+                Character("3D-Man", "https://pbs.twimg.com/media/EoQClRBXYAECWxN.jpg")
+            )
+        )
+    }
+
+    private fun initCharactersAdapter() {
+        with(binding.recyclerCharacters) {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
     }
 }
